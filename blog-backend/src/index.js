e5429a48de01b221e32a6204edf73ba727dadd4f
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-05-19 15:41:45 +0900

    Add : koa

---
+++ blog-backend/src/index.js
@@ -0,0 +1,26 @@
+const Koa = require("koa");
+
+const app = new Koa();
+
+app.use((ctx, next) => {
+  console.log(ctx.url);
+  console.log(1);
+  if (ctx.query.authorized !== "1") {
+    ctx.status = 401; // Unauthorized
+    return;
+  }
+  next();
+});
+
+app.use((ctx, next) => {
+  console.log(2);
+  next();
+});
+
+app.use((ctx) => {
+  ctx.body = "hello world";
+});
+
+app.listen(4000, () => {
+  console.log("Listening to port http://localhost:4000/");
+});
