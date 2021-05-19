3ff8189eee3999c57259a918bf1f28e5a7c80c7c
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-05-19 16:52:49 +0900

    Add : params , query

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -1,25 +1,28 @@
 const Koa = require("koa");
+const Router = require("koa-router");
 
 const app = new Koa();
+const router = new Router();
 
-app.use((ctx, next) => {
-  console.log(ctx.url);
-  console.log(1);
-  if (ctx.query.authorized !== "1") {
-    ctx.status = 401; // Unauthorized
-    return;
-  }
-  next();
+// 라우터 설정
+router.get("/", (ctx) => {
+  ctx.body = "홈";
 });
 
-app.use((ctx, next) => {
-  console.log(2);
-  next();
+router.get("/about/:name?", (ctx) => {
+  const { name } = ctx.params;
+  // name의 존재 유무에 따라 다른 결과 출력
+  ctx.body = name ? `${name}의 소개` : "소개";
 });
 
-app.use((ctx) => {
-  ctx.body = "hello world";
+router.get("/posts", (ctx) => {
+  const { id } = ctx.query;
+  // id의 존재 유뮤에 따라 다른 결과 출력
+  ctx.body = id ? `포스트 #${id}` : `포스트 아이디가 없습니다.`;
 });
+
+// app 인스턴스에 라우터 적용
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(4000, () => {
   console.log("Listening to port http://localhost:4000/");
