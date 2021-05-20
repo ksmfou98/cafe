e4813143e6d9c8902e799eee2df8d95e7d33142d
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-05-20 20:50:35 +0900

    Setting : 폴더 구조

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -1,25 +1,16 @@
 const Koa = require("koa");
 const Router = require("koa-router");
+const bodyParser = require("koa-bodyparser");
+
+const api = require("./api");
 
 const app = new Koa();
 const router = new Router();
 
-// 라우터 설정
-router.get("/", (ctx) => {
-  ctx.body = "홈";
-});
+router.use("/api", api.routes()); // api 라우터 적용
 
-router.get("/about/:name?", (ctx) => {
-  const { name } = ctx.params;
-  // name의 존재 유무에 따라 다른 결과 출력
-  ctx.body = name ? `${name}의 소개` : "소개";
-});
-
-router.get("/posts", (ctx) => {
-  const { id } = ctx.query;
-  // id의 존재 유뮤에 따라 다른 결과 출력
-  ctx.body = id ? `포스트 #${id}` : `포스트 아이디가 없습니다.`;
-});
+// 라우터 적용 전에 bodyParser 적용
+app.use(bodyParser());
 
 // app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
