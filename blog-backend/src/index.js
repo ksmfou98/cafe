bba9543c04720b3c297c16a2da766a316210846e
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-05-22 18:14:40 +0900

    Setting : backend setup

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -1,20 +1,13 @@
-const Koa = require("koa");
-const Router = require("koa-router");
-const bodyParser = require("koa-bodyparser");
+import express from "express";
 
-const api = require("./api");
+const app = express();
+const PORT = 4000;
 
-const app = new Koa();
-const router = new Router();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-router.use("/api", api.routes()); // api 라우터 적용
+const handleListening = () => {
+  console.log(`✅ Listening on : http://localhost:${PORT}`);
+};
 
-// 라우터 적용 전에 bodyParser 적용
-app.use(bodyParser());
-
-// app 인스턴스에 라우터 적용
-app.use(router.routes()).use(router.allowedMethods());
-
-app.listen(4000, () => {
-  console.log("Listening to port http://localhost:4000/");
-});
+app.listen(PORT, handleListening);
