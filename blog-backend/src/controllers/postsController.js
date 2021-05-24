e700d3ca574f5a0a2c5decea3082b8cbf68961e5
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-05-24 19:30:12 +0900

    Implements : post update, post delete

--- blog-backend/src/controllers/postsController.js
+++ blog-backend/src/controllers/postsController.js
@@ -1,6 +1,5 @@
 import Post from "../models/post";
 
-
 // 글 작성
 export const write = async (req, res) => {
   const post = new Post(req.body);
@@ -16,7 +15,7 @@ export const write = async (req, res) => {
   }
 };
 
-// 전체 글 조회 
+// 전체 글 조회
 export const list = async (req, res) => {
   try {
     const posts = await Post.find().exec();
@@ -48,6 +47,32 @@ export const read = async (req, res) => {
   }
 };
 
-export const remove = (req, res) => {};
+// 특정 글 삭제
+export const remove = async (req, res) => {
+  const id = req.params.id;
+  try {
+    await Post.findByIdAndRemove(id).exec();
+    res.status(204).json({ success: true }); // 204 : 성공하기는 했지만 응답할 데이터는 없음
+  } catch (e) {
+    res.status(500).send(e);
+  }
+};
 
-export const update = (req, res) => {};
+// 특정 글 업데이트
+export const update = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const post = await Post.findByIdAndUpdate(id, req.body, {
+      new: true,
+    }).exec(); // new 값을 설정하면 업데이트 된 데이터를 반환한다. false일 경우 업데이트 되기 전 데이터 반환
+    if (!post) {
+      return res.status(404).json({ success: false });
+    }
+    res.status(200).json({
+      success: true,
+      post,
+    });
+  } catch (e) {
+    res.status(500).send(e);
+  }
+};
