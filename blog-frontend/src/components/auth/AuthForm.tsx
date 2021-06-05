adb06a0a9af344d1116b6a74258d53d107668765
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-06-05 20:40:47 +0900

    Styles : Register Page UI

--- blog-frontend/src/components/auth/AuthForm.tsx
+++ blog-frontend/src/components/auth/AuthForm.tsx
@@ -89,12 +89,25 @@ const AuthForm = ({ type }: AuthFormProps) => {
           type="password"
         />
 
-        <ButtonWithMarginTop cyan fullWidth>
-          로그인
+        {type === 'register' && (
+          <StyledInput
+            autoComplete="new-password"
+            name="passwordConfirm"
+            placeholder="비밀번호 확인"
+            type="password"
+          />
+        )}
+
+        <ButtonWithMarginTop cyan fullWidth style={{ marginTop: '1rem' }}>
+          {text}
         </ButtonWithMarginTop>
       </form>
       <Footer>
-        <Link to="/register">회원가입</Link>
+        {type === 'login' ? (
+          <Link to="/register">회원가입</Link>
+        ) : (
+          <Link to="/login">로그인</Link>
+        )}
       </Footer>
     </AuthFormBlock>
   );
