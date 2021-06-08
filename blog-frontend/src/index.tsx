5906d9fa6410270641a17c588c9d0e8f5776f8ff
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-06-08 22:45:25 +0900

    Fix : 주석 수정

--- blog-frontend/src/index.tsx
+++ blog-frontend/src/index.tsx
@@ -7,12 +7,12 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
-// 내일 마무리
+
 // function loadUser() {
 //   try {
 //     let user = localStorage.getItem('user');
 //     console.log(user);
-//     if (!user) return; // 로그인 상태가 아니라면 아무것도 안함
+//     if (!user) return; // 로그인 상태가 아니라면 함수에서 나옴
 //   } catch (e) {}
 // }
 
